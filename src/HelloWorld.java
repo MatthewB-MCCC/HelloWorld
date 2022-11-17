6ce0b9306830fc277f3aee718e3befb5e56b2b3c
MatthewB-MCCC <mboehme6961@students.mc3.edu>
2022-11-16 20:45:53 -0500

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,20 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+        int number = 100;
 
-        System.out.println("Hello World");
+        System.out.println("Hello World, Numbers from 1 to " + number + ": ");
+        for (int i = 1; i <= number; i++) {
+            System.out.print(i + " ");
+
+
+        }
 
 
     }
 }
+
+
+
+
+
